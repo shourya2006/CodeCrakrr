54a5b33c88731fcb338d2b1566dd8d6d38b6459e
Shourya <75479705+shourya2006@users.noreply.github.com>
2025-05-08 21:33:48 +0530

    Deployement Fix

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../Static/logo.png";
+import logo from "../Static/Logo.png";
 import { AuthContext } from "../context/AuthContext";
 import { HandleContext } from "../context/HandleContext";
 const Signup = () => {
