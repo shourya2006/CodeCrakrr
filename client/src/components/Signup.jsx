3dee4c8bd1dde06b68617170c1fc591c61518fc2
Shourya <75479705+shourya2006@users.noreply.github.com>
2025-05-09 21:35:29 +0530

    Added loaders

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -4,6 +4,7 @@ import logo from "../Static/Logo.png";
 import { AuthContext } from "../context/AuthContext";
 import { HandleContext } from "../context/HandleContext";
 const Signup = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const { setToken } = useContext(AuthContext);
   const { handle } = useContext(HandleContext);
   const navigate = useNavigate();
@@ -13,8 +14,8 @@ const Signup = () => {
     password: "",
   });
   const [confirmPassword, setConfirmPassword] = useState("");
-  const VITE_API_URL= import.meta.env.VITE_API_URL || "";
-  
+  const VITE_API_URL = import.meta.env.VITE_API_URL || "";
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     if (name === "confirmPassword") {
@@ -29,8 +30,10 @@ const Signup = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     if (formData.password !== confirmPassword) {
       alert("Passwords do not match");
+      setIsLoading(false);
       return;
     }
     const response = await fetch(`${VITE_API_URL}/api/auth/register`, {
@@ -41,7 +44,7 @@ const Signup = () => {
       body: JSON.stringify(formData),
     });
     const data = await response.json();
-    console.log(data);
+
     if (
       data.error &&
       data.error.includes(
@@ -49,10 +52,13 @@ const Signup = () => {
       )
     ) {
       alert("Email already exists");
+      setIsLoading(false);
+
       return;
     }
     if (data.errors) {
       alert(data.errors[0].msg);
+      setIsLoading(false);
       return;
     }
     if (response.ok) {
@@ -64,6 +70,7 @@ const Signup = () => {
     } else {
       alert("server error. Refresh the page and try again");
     }
+    setIsLoading(false);
   };
 
   return (
@@ -168,12 +175,39 @@ const Signup = () => {
             </div>
 
             <div className="pt-2">
-              <button
-                type="submit"
-                className="w-full py-2 px-4 rounded-md cursor-pointer text-white font-medium bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-              >
-                Sign up
-              </button>
+              {isLoading ? (
+                <button
+                  disabled
+                  type="button"
+                  class="w-full py-2 px-4 rounded-md cursor-pointer text-white font-medium bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                >
+                  <svg
+                    aria-hidden="true"
+                    role="status"
+                    class="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  Signing up...
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="w-full py-2 px-4 rounded-md cursor-pointer text-white font-medium bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                >
+                  Sign up
+                </button>
+              )}
             </div>
           </form>
 
